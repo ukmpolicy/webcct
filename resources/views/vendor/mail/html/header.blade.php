478b61cd7e649dd345e95f3d340778d84e319b73
zulfahmi <=>
2022-02-17 23:13:09 +0700

    Add Blue Logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="https://cct.ukmpolicy.org/images/logo_b.svg" class="logo" alt="Logo Cyber Competition And Talkshow">
+    <img src="https://cct.ukmpolicy.org/images/logo_b.png" class="logo" alt="Logo Cyber Competition And Talkshow">
     {{ $slot }}
 </a>
 </td>

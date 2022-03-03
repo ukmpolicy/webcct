b85dd0e3f2f03ac8ec96035330824d51d6d19a2a
zulfahmi <=>
2022-03-03 10:36:57 +0700

    Memperbaiki mail untuk competition

--- resources/views/mails/accepted.blade.php
+++ resources/views/mails/accepted.blade.php
@@ -2,11 +2,7 @@
 
 <h2 class="ticket-competition">Ticket Cyber Competition And Talkshow</h2>
 
-<p>Hai {{ $registration->name }}, Terima kasih telah mendaftar diri anda pada acara Cyber Competion And Talkshow. Berikut adalah nomor serial anda:</p>
-
-<div class="serial">{{ $registration->serial_number }}</div><br>
-
-<p>Jika anda adalah peserta offline, anda harus menunjukan email ini saat rechecking di tempat acara. Nomor serial ini dapat digunakan untuk absensi pada saat acara berlangsung. Untuk informasi lebih lanjut anda dapat mengunjungi sosial media kami atau dapat menghubungi kontak person berikut:</p>
+<p>Hai {{ $registration->name }}, Terima kasih telah mendaftar diri anda pada acara Cyber Competion And Talkshow. Untuk informasi lebih lanjut anda dapat mengunjungi sosial media kami atau dapat menghubungi kontak person berikut:</p>
 
 <ul>
     <li>Ayatullah R. K. : 0822-7780-0517</li>
